dbd7a5ddfb01edaebd9add1c86cc83f5377da717
DimaPhil <dima239philippov@gmail.com>
2015-05-29 05:15:51 +0300

    5 hometask seems to be done

---
+++ simplesh/simplesh.c
@@ -0,0 +1,112 @@
+#include "helpers.h"
+#include "bufio.h"
+
+#define MAX_BUF_SIZE 4096
+#define MAX_ARGUMENTS 2048
+
+int spaceOrDelimeter(char c, char delimeter) {
+    return c == delimeter || c == '\0' || c == ' ' || c == '\t';
+}
+
+int countSymbol(char *buffer, char delimeter) {
+    if (buffer[0] == '\0') {
+        return 0;
+    }
+    int count = 0;
+    int i = 1;
+    while (1) {
+        if (!spaceOrDelimeter(buffer[i - 1], delimeter) && spaceOrDelimeter(buffer[i], delimeter)) {
+            count++;
+        }
+        if (buffer[i] == '\0' || buffer[i] == delimeter) {
+            return count;
+        }
+        i++;
+    }
+}
+
+char* getLexem(char *buffer, char *delimeter, int *newPosition) {
+    int i = 0;
+    while (buffer[i] == ' ' || buffer[i] == '\t') {
+        i++;
+    }
+    int start = i;
+    while (buffer[i] != ' ' && buffer[i] != '\t' && buffer[i] != '\0' && buffer[i] != *delimeter) {
+        i++;
+    }
+    int end = i;
+    *delimeter = buffer[i];
+    *newPosition = end;
+    if (start == end) {
+        return 0;
+    }
+    return strndup(buffer + start, end - start); 
+}
+
+int getDelimeter(char *buffer, char delimeter) {
+    int i = 0; 
+    while (buffer[i] == ' ' || buffer[i] == '\t') {
+        i++;
+    }
+    if (buffer[i] == delimeter) {
+        i++;
+    }
+    return i;
+}
+
+void sigint_handler(int signal) {
+}
+
+int main() {
+    struct sigaction action;
+    memset(&action, '\0', sizeof action);
+    action.sa_handler = &sigint_handler;
+   
+    if (sigaction(SIGINT, &action, NULL) < 0) {
+        return 1;
+    }
+ 
+    struct buf_t *entry = buf_new(MAX_BUF_SIZE); 
+    while (1) {
+        if (write_(STDOUT_FILENO, "$ ", 2) == -1) {
+            return 1;
+        }
+        int position = buf_read_until(STDIN_FILENO, entry, '\n');
+        if (position == -2) {
+            if (write_(STDOUT_FILENO, "\n$ ", 3) == -1) {
+                return 1;
+            }
+            continue;
+        }
+        if (position == -3) {
+            return 0;
+        }
+        char *buffer = entry->buffer;        
+        buffer[position] = '\0';
+        struct execargs_t *arguments[MAX_ARGUMENTS];
+        int argumentIndex = 0;
+        while (1) {
+            char delimeter = '|';
+            int argc = countSymbol(buffer, delimeter);
+            if (argc == 0) {
+                break;
+            }
+            char *argv[argc]; 
+            int shift;
+            int i;
+            for (i = 0; i < argc; i++) {
+                delimeter = '|';
+                argv[i] = getLexem(buffer, &delimeter, &shift);
+                buffer += shift;
+            }
+            arguments[argumentIndex] = (struct execargs_t*) malloc(sizeof(struct execargs_t));
+            *arguments[argumentIndex] = getExecargs(argc, argv);  
+            shift = getDelimeter(buffer, '|'); 
+            buffer += shift;
+            argumentIndex++;
+        }
+        entry->size -= (buffer - entry->buffer + 1);
+        runpiped(arguments, argumentIndex);
+    }
+    return 0;
+}
